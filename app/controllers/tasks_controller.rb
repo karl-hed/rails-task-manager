c6ebc36890f8eb300590eb8be37e5a629cba96b6
Karl Hedayati <k.hedayati.10@gmail.com>
2022-11-22 13:22:14 -0500

    added create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     # raise
     @tasks = Task.all
@@ -6,5 +8,28 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+    # @task = Task.new(task_params)
+    # @task.save
+    # redirect_to task_path(@task)
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
